4ca0a57bd03e2f8d68d7620f5c6e93cc67fdbc53
Grant Murphy <grant.murphy@hpe.com>
2015-12-04 15:04:37 -0800

    Add license information

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+/*
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
 #![feature(custom_derive, custom_attribute, plugin)]
 #![plugin(serde_macros)]
 #![cfg_attr(test, allow(dead_code, unused_imports))]
